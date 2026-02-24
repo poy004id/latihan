05e98322dcae06ab289061b150bc0a0b7ceb6b6a
Agung Supoyo <agungsupoyo@gmail.com>
2026-02-24 14:19:50 +0700

    Add AuthContext for user authentication management

---
+++ context/AuthContext.js
@@ -0,0 +1,70 @@
+// contexts/AuthContext.js
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { createContext, useContext, useEffect, useState } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    loadUser();
+  }, []);
+
+  const loadUser = async () => {
+    try {
+      const storedUser = await AsyncStorage.getItem("user");
+      if (storedUser) {
+        setUser(JSON.parse(storedUser));
+      }
+    } catch (error) {
+      console.log("Load user error:", error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const login = async (username, password) => {
+    try {
+      setLoading(true);
+
+      const response = await axios.post("https://api.mockfly.dev/mocks/838461a3-ce16-45c6-ad65-3c409d160449/login", {
+        username,
+        password,
+      });
+      const token = response.data.token;
+      const userData = response.data.user;
+
+      await AsyncStorage.setItem("token", token);
+
+      setUser(userData);
+      return { success: true };
+    } catch (error) {
+      return { success: false, message: "Login failed" };
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const logout = async () => {
+    await AsyncStorage.removeItem("user");
+    setUser(null);
+  };
+
+  return (
+    <AuthContext.Provider
+      value={{
+        user,
+        loading,
+        login,
+        logout,
+        isAuthenticated: !!user,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
